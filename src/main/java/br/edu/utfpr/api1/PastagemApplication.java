b8fd9c831d26c65e04ff283e56794a06edf29176
Gouveia <gouveia.j.vitor@gmail.com>
2025-04-16 08:16:14 -0300

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ src/main/java/br/edu/utfpr/api1/PastagemApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.api1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastagemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastagemApplication.class, args);
+    }
+
+}
